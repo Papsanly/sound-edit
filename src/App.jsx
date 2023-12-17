8f170a9b5aacea6403884c274da7a17596afb038
Papsan <papsanlysenko@gmail.com>
2023-12-17 21:47:19 +0200

    setup prettier

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ export default function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button onClick={() => setCount(count => count + 1)}>
           count is {count}
         </button>
         <p>
