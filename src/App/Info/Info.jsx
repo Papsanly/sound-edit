ec86506cf8257b1627898ae92015893301105be4
Papsan <papsanlysenko@gmail.com>
2023-12-22 20:14:08 +0200

    implemented display for current time

--- src/App/Info/Info.jsx
+++ src/App/Info/Info.jsx
@@ -1,5 +1,17 @@
 import style from './Info.module.css'
+import { useSelector } from 'react-redux'
+import { select } from '@/store/app.js'
 
 export default function Info() {
-  return <p className={style.time}>00:00:01:89</p>
+  const currentTime = useSelector(select).currentTime
+
+  const seconds = Math.round(currentTime / 1000)
+  const minutes = Math.round(seconds / 60)
+  const hours = Math.round(minutes / 60)
+
+  return (
+    <p className={style.time}>
+      {hours}:{minutes}:{seconds}:{currentTime}
+    </p>
+  )
 }
